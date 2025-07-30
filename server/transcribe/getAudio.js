24c9fa5552ea33a41752bf45b1e397045c01f620
Yash Yeltiwar <boostbrain2021@gmail.com>
2025-07-30 23:08:43 +0530

    modified getAudio.js and getTranscript.js to get audio and transcript using deepgram.

--- server/transcribe/getAudio.js
+++ server/transcribe/getAudio.js
@@ -2,14 +2,14 @@ const ytdl = require('ytdl-core')
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 const ffmpeg = require("fluent-ffmpeg")
 ffmpeg.setFfmpegPath(ffmpegPath)
-const videURL = "https://www.youtube.com/watch?v=_GaY21NF4-U"
+const videURL = "https://www.youtube.com/watch?v=0CmtDk-joT4"
 const audio = ytdl(videURL,{
     quality: "highestaudio",
     filter: "audioonly"
 });
 ffmpeg(audio)
     .toFormat('mp3')
-    .saveToFile("audio.mp3", (stdout, stderr) => {})
+    .saveToFile("audio2.mp3", (stdout, stderr) => {})
     .on("end",() => {
     console.log("finish converting to mp3")
     })
